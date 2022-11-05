1a88eb8dcadbc31b1b6ece8480d50a3351a16422
Daniel Nizynski <danielnizynski97@gmail.com>
2022-11-05 18:08:55 +0100

    added dist folder

---
+++ dist/lib/CustomCounter/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import { CustomCounterProps } from './types';
+declare const CustomCounter: ({ initialValue }: CustomCounterProps) => JSX.Element;
+export default CustomCounter;
---
+++ dist/lib/CustomCounter/types.d.ts
@@ -0,0 +1,3 @@
+export interface CustomCounterProps {
+    initialValue?: number;
+}
